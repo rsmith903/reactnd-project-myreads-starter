f01bd4480e04421ab441cacfec6a999f3cd3438c
rsmith903 <39638319+rsmith903@users.noreply.github.com>
2018-09-05 07:26:03 -0500

    Create bookshelves.js

---
+++ src/bookshelves.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Book from './Book';
+
+class BookShelf extends Component {
+  render() {
+    const { shelfName, books, changeShelf } = this.props;
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  changeShelf={changeShelf} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookShelf;
